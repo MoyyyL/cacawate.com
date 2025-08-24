bbb02830c1188b8223925f24bed4cc343bd858ab
MoyyyL <moyyyl0526@gmail.com>
2025-08-23 19:36:25 -0600

    formatting the page and adding the icon

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ var TxtType = function (el, toRotate, period) {
     this.el = el;
     this.loopNum = 0;
     this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
+    this.txt = "";
     this.tick();
     this.isDeleting = false;
 };
@@ -32,7 +32,7 @@ TxtType.prototype.tick = function () {
         this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
 
-    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
 
     var that = this;
     var delta = 200 - Math.random() * 100;
@@ -44,7 +44,7 @@ TxtType.prototype.tick = function () {
     if (!this.isDeleting && this.txt === fullTxt) {
         delta = this.period;
         this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
+    } else if (this.isDeleting && this.txt === "") {
         this.isDeleting = false;
         this.loopNum++;
         delta = 500;
@@ -56,18 +56,18 @@ TxtType.prototype.tick = function () {
 };
 
 window.onload = function () {
-    var elements = document.getElementsByClassName('personal__names');
+    var elements = document.getElementsByClassName("personal__names");
     for (var i = 0; i < elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
+        var toRotate = elements[i].getAttribute("data-type");
+        var period = elements[i].getAttribute("data-period");
         if (toRotate) {
             new TxtType(elements[i], JSON.parse(toRotate), period);
         }
     }
     // INJECT CSS
-    var css = document.createElement('style');
-    css.type = 'text/css';
+    var css = document.createElement("style");
+    css.type = "text/css";
     css.innerHTML =
-        '.personal__names > .wrap { border-right: 0.08em solid #fff}';
+        ".personal__names > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
 };
